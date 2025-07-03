eb5cbc2b7ac44f9330ee6f35f43ac9bc169310cb
Isurika Gunasekara <gunasekaratiu.22@uom.lk>
2025-07-03 21:00:08 +0530

    Add initial implementation of CMake configuration, license, launch file, and package.xml for task_05

---
+++ ros_ws/src/task_05/launch/launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tests',
+            executable='chatter',
+            name='test_chatter',
+            output='screen',
+            remappings={
+                ('/chatter', '/test/chatter'),
+            },
+        ),
+        Node(
+            package='tests',
+            executable='listener',
+            name='listener',
+            output='screen',
+        ),
+
+        ExecuteProcess(
+            cmd=[
+                'ros2', 'topic', 'pub', '--once', '/twist', 'geometry_msgs/msg/Twist',
+                '{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'
+            ],
+            output='screen'
+        ),
+    ])
